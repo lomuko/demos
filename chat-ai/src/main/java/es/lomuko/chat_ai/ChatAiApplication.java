ee054f3f218ffb27be951eb77aa03bba9319ff29
David Tortajada <mail@davidtor.es>
2026-05-07 18:58:02 +0200

    chat-ai

---
+++ chat-ai/src/main/java/es/lomuko/chat_ai/ChatAiApplication.java
@@ -0,0 +1,13 @@
+package es.lomuko.chat_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAiApplication.class, args);
+	}
+
+}
